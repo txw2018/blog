3d6f135620f07f629306e9befbc86678d6726b46
txw <1658172479@qq.com>
2022-01-22 11:21:08 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: 'Tang的学习笔记',
+    description: '平时的一些学习记录',
+    base:'/blog/',
+    themeConfig: {
+        logo: '/images/logo.jpg',
+        navbar: [
+            { text: 'TypeScript', link: '/typescript' },
+            { text: 'github', link: 'https://github.com/txw2018' },
+          
+        ]
+    }
+  }
